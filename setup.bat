9dcebc0e0412093dc60ff389c11e8c44879d044d
Kushagra Mehrotra <68564934+kushaagr@users.noreply.github.com>
2022-07-16 22:54:08 +0530

    Update file

--- setup.bat
+++ setup.bat
@@ -7,23 +7,27 @@ IF /I %PROCESSOR_ARCHITECTURE% == AMD64 (
 )
 echo TESS_DRIVER = r'%TESSPATH%' > config.py
 echo BROW_DRIVER = '%USERPROFILE:\=/%/.wdm/drivers/chromedriver/win32/ DRIVER_VERSION /chromedriver' >> config.py
+rem start tesseract-setup
+ECHO REQUIREMENT: Tesseract-OCR — download from
+ECHO              https://github.com/UB-Mannheim/tesseract/wiki && ECHO. 
 
 python --version 2>NUL
 IF errorlevel 1 (
     IF /I %PROCESSOR_ARCHITECTURE% == AMD64 (
-rem         python-3.10.4-amd64.exe
-		echo UNSATISFIED REQUIREMENT: PYTHON X86
+        rem python-3.10.4-amd64.exe
+		ECHO UNSATISFIED REQUIREMENT: PYTHON X86
+		ECHO INSTALL PYTHON ^(X86^)
     ) ELSE (
-rem         python-3.10.4.exe
-		echo UNSATISFIED REQUIREMENT: PYTHON
+        rem python-3.10.4.exe
+		ECHO UNSATISFIED REQUIREMENT: PYTHON
+		ECHO INSTALL PYTHON
     )
 ) ELSE (
-    start pip install --user -r requirements.txt
+	ECHO is installed.
+    start cmd /C "pip install --user -r requirements.txt & TIMEOUT /T 10"
+    ECHO. && ECHO Setup complete.
 )
 
-rem start tesseract-setup
-ECHO REQUIREMENT: Tesseract-OCR 
-ECHO (download from https://github.com/UB-Mannheim/tesseract/wiki)
 PAUSE
 
 REM SET BROWDRIVERPATH=%USERPROFILE:\=/%.wdm/drivers/chromedriver/win32/99.0.4844.51/chromedriver
